f3c130fb3d366fa6b05a7349603326db1de3c0ac
CaoDucDao <caoducdao17022005@gmail.com>
2025-12-10 22:54:25 +0700

    merge

--- onlinecourse/onlinecourse/assets/js/script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded',function(){
-    const alerts=document.querySelectorAll('.alert');
-    alerts.forEach(a=>setTimeout(()=>a.style.display='none',3000));
-});
